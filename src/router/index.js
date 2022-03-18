b7a97958fc2081654a2e7c7d67cced47057cfa5f
ArtcalO <niyca14@gmail.com>
2022-03-18 10:21:02 +0100

    convert from ts to js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes= [
   {
     path: '/',
     redirect: '/home'
